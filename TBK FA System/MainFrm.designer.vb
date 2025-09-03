8f12972f793837925c88d72042440ba7846ffc76
TBKT-SYSTEM <117958477+TBKT-SYSTEM@users.noreply.github.com>
2025-09-03 09:20:27 +0700

    Update MainFrm.designer.vb
    READY TT001

--- TBK FA System/MainFrm.designer.vb
+++ TBK FA System/MainFrm.designer.vb
@@ -99,9 +99,9 @@ Partial Class MainFrm
         Me.Label9.ForeColor = System.Drawing.Color.White
         Me.Label9.Location = New System.Drawing.Point(576, 136)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(201, 19)
+        Me.Label9.Size = New System.Drawing.Size(228, 19)
         Me.Label9.TabIndex = 23
-        Me.Label9.Text = "FA VERSIONS 2.1.7 TD"
+        Me.Label9.Text = "FA VERSIONS 2.1.7 TT001"
         '
         'Panel1
         '
